227e6d6c1371d6ffd0eceb9b571c4ab0c78cb323
BuiThongHoangDuc <cubiheo5@gmail.com>
2021-02-02 15:50:46 +0700

    CORS Update

--- Capstone API V2/Startup.cs
+++ Capstone API V2/Startup.cs
@@ -32,6 +32,17 @@ namespace Capstone_API_V2
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<FamilyDoctorContext>(options => options.UseSqlServer(Configuration.GetConnectionString("Capstone_DB")));
+            services.AddCors(Options =>
+            {
+                Options.AddPolicy(
+                    name: "CorsPolicy",
+                    builder =>
+                    {
+                        builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader();
+                    });
+            });
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
