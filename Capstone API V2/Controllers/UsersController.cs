98906b7ec624f69a2af82f6b145eec918864cbbe
Tran Phu Tai <taitpse130083@fpt.edu.vn>
2021-04-13 09:52:22 +0700

    Fix update user api

--- Capstone API V2/Controllers/UsersController.cs
+++ Capstone API V2/Controllers/UsersController.cs
@@ -80,14 +80,16 @@ namespace Capstone_API_V2.Controllers
         }
 
         [HttpPut]
-        public async Task<IActionResult> Update([FromBody] UserModel model)
+        public async Task<IActionResult> Update([FromBody] UserModel model, bool isAcceptDoctor)
         {
             var result = await _userService.UpdateUser(model);
 
-            var doctor = await _userService.GetAsync(1,1,filter: f => f.AccountId == model.AccountId, includeProperties: "Profiles");
-
-            await _userService.SendEmailAsync(doctor.SingleOrDefault().Profiles.SingleOrDefault().Email, doctor.SingleOrDefault().Profiles.SingleOrDefault().FullName, model.Waiting, model.Disabled);
-            return Ok(result);
+            if (isAcceptDoctor)
+            {
+                var doctor = await _userService.GetAsync(1, 1, filter: f => f.AccountId == model.AccountId, includeProperties: "Profiles");
+                await _userService.SendEmailAsync(doctor.SingleOrDefault().Profiles.SingleOrDefault().Email, doctor.SingleOrDefault().Profiles.SingleOrDefault().FullName, model.Waiting, model.Disabled);
+            }
+            return Ok(isAcceptDoctor);
         }
     }
 }
